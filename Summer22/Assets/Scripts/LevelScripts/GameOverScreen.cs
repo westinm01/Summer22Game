fc75702ea9b6f605cc47e0ee1a6dddc6bb8060fc
N-a-n-0 <84410640+N-a-n-0@users.noreply.github.com>
2022-11-11 23:26:59 -0800

    Game Over Added to all last Lvs
    NICE

---
+++ Summer22/Assets/Scripts/LevelScripts/GameOverScreen.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameOverScreen : MonoBehaviour
+{
+
+    [SerializeField] GameObject panel;
+
+    [SerializeField] Image timeImage;
+
+    [SerializeField] Text timeText;
+
+    [SerializeField] float duration, currentTime;
+
+    void Start()
+    {
+        panel.SetActive(false);
+        currentTime = duration;
+        timeText.text = currentTime.ToString();
+        StartCoroutine(TimeIEn());
+    }
+
+    IEnumerator TimeIEn()
+    {
+        while (currentTime >= 0)
+        {
+            timeImage.fillAmount = Mathf.InverseLerp(0, duration, currentTime);
+            timeText.text = currentTime.ToString();
+            yield return new WaitForSeconds(1f);
+            currentTime--;
+        }
+        OpenPanel();
+    }
+
+    void OpenPanel()
+    {
+        timeText.text = "";
+        panel.SetActive(true);
+        Time.timeScale = 0;
+    }
+}
